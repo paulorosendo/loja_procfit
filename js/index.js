9630913a3ef02328e1c1c337bd46ee0c96cb8b24
Paulo Rosendo <henriquerosendo10@outlook.com>
2022-10-31 14:59:22 -0300

    ATT 31.10

--- js/index.js
+++ js/index.js
@@ -1,3 +1,14 @@
+function pulaLinha() {
+
+    document.write("<br>");
+}
+
+function mostra(frase) {
+
+    document.getElementById("root").innerHTML = (frase)
+
+}
+
 
 // header
 mostra(`
@@ -20,13 +31,76 @@ mostra(`
             </ul>
 
             <img id="img-logo" src="img/logo-proc.png">
-            <p class='txt-logo'>STORE</p>
+            <p class='txt-logo'>Store</p>
 
             </div>
         <div>
-    </header>    
+    </header> 
+    
+    
+    <main>
+        <div class='text-aling-center'>
+            <h1 class="bemvindo">Bem vindo(a), ProcStore!!
+        </div>
+        <div >
+            <ul id="container" >
+                <li class="produtos">
+                    <div class="card">
+                        <a href="">
+                            <img class="img1" src="img/Camisa SQL.jpg"/>
+                            <p>Descrição: Cadeno / agenda Procfit. com 100 folhas formato capa dura esperial</p>
+
+                            <h3 class="descricao-produto">Caderno Procfit</h3>
+                            <h3>R$25,00</h3>
+                        </a>
+                    </div>
+                </li>
+
+
+                <li class="produtos">
+                    <div class="card">
+                        <img class="img1" src="img/Caderno.jpg"/>
+                        <p>Cadeno/Agenda Procfit com 100 folhas, formato capa dura, esperial</p>
+                        <h3 class="preco-produto">R$25,00</h3>
+                        <div class="addCar">
+                            <h2>Adicionar o ao carrinho </h2>
+                        </div>
+                    </div>
+                </li>
+                
+                
+                <li class="produtos">
+                    <div class="card">
+                        <a href="">
+                            <img class="img1" src="img/Camisa Seja Diferente.jpg"/>
+                            <p>Descrição: Cadeno / agenda Procfit. com 100 folhas formato capa dura esperial</p>
+
+                            <h3 class="descricao-produto">Caderno Procfit</h3>
+                            <h3>R$25,00</h3>
+                        </a>
+                    </div>
+                </li>   
+                
+                <li class="produtos">
+                    <div class="card">
+                        <a href="">
+                            <img class="img1" src="img/Caderno.jpg"/>
+                            <p>Descrição: Cadeno / agenda Procfit. com 100 folhas formato capa dura esperial</p>
+
+                            <h3 class="descricao-produto">Caderno Procfit</h3>
+                            <h3>R$25,00</h3>
+                        </a>
+                    </div>
+                </li>  
+
+            </ul>
+        </div
+    </main>
     `
 )
+
+
+
 
 
 
